6cbe4256b98126a6dff0fc9f110b0afc9383c4dc
+ <+>
2024-03-20 10:32:26 +0300

    used built-in objects llike Math and Number to solve complex tasks

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+
+const random = Math.floor(Math.random()* Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random);
+
+let num3 = 20;
+let num4 = 5;
+
+const mod = num3 / num4;
+console.log(mod);
+
+const Group1 =[11,13,1,6,20];
+const max = Math.max(...Group1);
+
+console .log(max);
